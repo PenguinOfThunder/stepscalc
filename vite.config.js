b5f93d74c5b4bd5b80160bc9a43cfdbc2e31fcdb
PenguinOfThunder <PenguinOfThunder@users.noreply.github.com>
2025-04-18 02:46:54 +0000

    Add html coverage report

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   test: {
     coverage: {
-      reporter: ["text", "json-summary", "json"],
+      reporter: ["text", "json-summary", "json", "html"],
       reportOnFailure: true
     }
   },
